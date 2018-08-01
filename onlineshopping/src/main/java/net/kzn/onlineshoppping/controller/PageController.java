f1885ea7c6c38c273586658c53946ef35b2751b1
Gaurabh <Gaurabh@DESKTOP-4VIUI5K>
2018-08-02 01:02:50 +0530

    Added Files for Spring MVC Flow.
    PageController.java for page controller.
    page.jsp to redirect views.

---
+++ onlineshopping/src/main/java/net/kzn/onlineshoppping/controller/PageController.java
@@ -0,0 +1,19 @@
+package net.kzn.onlineshoppping.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class PageController {
+	
+	@RequestMapping(value ={"/","/home","/index"})
+	public ModelAndView index() {
+		
+		ModelAndView mv = new ModelAndView();
+		mv.addObject("greeting", "Welcome to Spring Web MVC Message");
+		return mv;
+			
+	}
+
+}
