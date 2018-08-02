412b4968770fd5f09d0c23162bc8ba9267ac5e75
Gaurabh <Gaurabh@DESKTOP-4VIUI5K>
2018-08-03 03:10:50 +0530

    required-error String parameter "greeting" not present

--- onlineshopping/src/main/java/net/kzn/onlineshoppping/controller/PageController.java
+++ onlineshopping/src/main/java/net/kzn/onlineshoppping/controller/PageController.java
@@ -2,6 +2,7 @@ package net.kzn.onlineshoppping.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -14,6 +15,14 @@ public class PageController {
 		mv.addObject("greeting", "Welcome to Spring Web MVC Message");
 		return mv;
 			
+	}
+	
+	@RequestMapping(value = "/test")
+	public ModelAndView test(@RequestParam("greeting") String greeting){
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("greeting",greeting);
+		return mv;
+		
 	}
 
 }
