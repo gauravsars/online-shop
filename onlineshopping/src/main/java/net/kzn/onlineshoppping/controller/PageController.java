d5720e369757e53d8cfbdac7d1c5a3caaa97c58a
Gaurabh <Gaurabh@DESKTOP-4VIUI5K>
2018-08-03 08:58:45 +0530

    Added Bootstrap Files for loading the static content of the web-page. procedure download the latest version of bootstrap. copy paste the js , css and font files in the directory. go to startbootstrap.com download the shop online project of the same. replace the file content of page.jsp with the source file code.

--- onlineshopping/src/main/java/net/kzn/onlineshoppping/controller/PageController.java
+++ onlineshopping/src/main/java/net/kzn/onlineshoppping/controller/PageController.java
@@ -1,9 +1,7 @@
 package net.kzn.onlineshoppping.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -13,8 +11,9 @@ public class PageController {
 	public ModelAndView index() {
 		ModelAndView mv = new ModelAndView("page");
 		mv.addObject("greeting", "Welcome to Spring Web MVC Message");
-		return mv;
-			
+		return mv;	
 	}
+	
+	
 	
 }
