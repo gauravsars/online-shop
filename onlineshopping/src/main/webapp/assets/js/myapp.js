fd175246b596a45171f185611bfe80b4a15d16e3
Gaurabh <Gaurabh@DESKTOP-4VIUI5K>
2018-08-19 07:11:36 +0530

    Added dataTables properties tp configure menu items , by default total number of records.

--- onlineshopping/src/main/webapp/assets/js/myapp.js
+++ onlineshopping/src/main/webapp/assets/js/myapp.js
@@ -19,24 +19,29 @@ $(function() {
 	//code for jquery dataTable
 	//create a dataset
 
-	
+	var products = [
+	            	['1','ABC'],
+	            	['1','DEF'],
+	            	['1','GHI'],
+	            	['1','JKL'],
+	            	['1','MNO'],
+	            	['1','PQR'],
+	            	['1','STU'],
+	            	['1','VWX'],
+	            	['1','YZA'],
+	            	['1','BCA']
+	            ];
+
+	var $table = $('#productListTable');
+	//execute the below code only where we have this table.
+
+	if($table.length) {
+		$table.DataTable({
+			lengthMenu:[[3,5,10,-1],['3 Records','5 Records','10 Records','All Records']],
+			data:products,
+			pageLength:8
+		});
+		
+	}
 });
-var products = [
-            	['1','ABC'],
-            	['1','ABC'],
-            	['1','ABC'],
-            	['1','ABC'],
-            	['1','ABC'],
-            	['1','ABC'],
-            	['1','ABC'],
-            	['1','ABC'],
-            	['1','ABC'],
-            	['1','ABC']
-            ];
 
-var $table = $('#productListTable');
-//execute the below code only where we have this table.
-
-if($table.length) {
-	console.log('Inside the table !');
-}
