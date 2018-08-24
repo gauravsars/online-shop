a2aea7e63f0ac64c981570de780535a0282ce079
Gaurabh <Gaurabh@DESKTOP-4VIUI5K>
2018-08-24 11:44:34 +0530

    Added ExceptionHandler for handling not mapped urls. Added: GlobalDefaultExceptionHandler dispatcher-servlet.xml(ppp) error.jsp

---
+++ onlineshopping/src/main/java/net/kzn/onlineshoppping/exception/GlobalDefaultExceptionHandler.java
@@ -0,0 +1,29 @@
+package net.kzn.onlineshoppping.exception;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.NoHandlerFoundException;
+
+@ControllerAdvice
+public class GlobalDefaultExceptionHandler {
+
+	
+	@ExceptionHandler(NoHandlerFoundException.class)
+	public ModelAndView handlerNoHandlerFoundException() {
+		
+		ModelAndView mv = new ModelAndView("error");
+		
+		mv.addObject("errorTitle", "The page is not constructed!");
+		
+		mv.addObject("errorDescription", "The page you are looking for is not available now!");
+		
+		mv.addObject("title", "404 Error Page");
+		
+		return mv;
+	}
+	
+}
